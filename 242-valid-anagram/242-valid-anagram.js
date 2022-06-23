2476c4f7dd24178423775f0fc24f2b45a2422b0d
Ahmed Tarek El Nashar <a7medelnashar@hotmail.com>
2022-06-23 16:41:08 +0200

    Time: 104 ms (68.51%), Space: 49.8 MB (5.13%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,20 +4,34 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let firstMap = {};
-    let secondMap = {};
     
-    if (s.length !== t.length) return false;
+// Time Complexity = O(s+t)
+// Space Complexity = O(s+t)
     
-    for (let i = 0; i < s.length; i++) {
-        firstMap[s[i]] = !firstMap[s[i]] ? firstMap[s[i]] = 1 : firstMap[s[i]]+=1;
-        secondMap[t[i]] = !secondMap[t[i]] ? secondMap[t[i]] = 1 : secondMap[t[i]]+=1;
-    }
+//     ---------------------------------------------------------------------------      //
     
-    for (let char in firstMap) {
-        if (firstMap[char] !== secondMap[char]) return false;
-    }
+//     let firstMap = {};
+//     let secondMap = {};
     
-    return true;
+//     if (s.length !== t.length) return false;
+    
+//     for (let i = 0; i < s.length; i++) {
+//         firstMap[s[i]] = !firstMap[s[i]] ? firstMap[s[i]] = 1 : firstMap[s[i]]+=1;
+//         secondMap[t[i]] = !secondMap[t[i]] ? secondMap[t[i]] = 1 : secondMap[t[i]]+=1;
+//     }
+    
+//     for (let char in firstMap) {
+//         if (firstMap[char] !== secondMap[char]) return false;
+//     }
+    
+//     return true;
+    
+//     ---------------------------------------------------------------------------      //
+
+// Time Complexity = O(n log n) if we used better sorting algorithm
+// Space Complexity = O(1)
+
+    return s.split('').sort().join('') === t.split('').sort().join('')
+    
     
 };
