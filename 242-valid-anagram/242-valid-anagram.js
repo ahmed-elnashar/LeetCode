74fe7c34069fca0e6223932ac0c7597d9cde290f
Ahmed Tarek El Nashar <a7medelnashar@hotmail.com>
2022-06-23 16:29:14 +0200

    Time: 158 ms (16.56%), Space: 43.6 MB (66.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -9,12 +9,9 @@ var isAnagram = function(s, t) {
     
     if (s.length !== t.length) return false;
     
-    for (let char of s) {
-        firstMap[char] = !firstMap[char] ? firstMap[char] = 1 : firstMap[char]+=1;
-    }
-    
-    for (let char of t) {
-        secondMap[char] = !secondMap[char] ? secondMap[char] = 1 : secondMap[char]+=1;
+    for (let i = 0; i < s.length; i++) {
+        firstMap[s[i]] = !firstMap[s[i]] ? firstMap[s[i]] = 1 : firstMap[s[i]]+=1;
+        secondMap[t[i]] = !secondMap[t[i]] ? secondMap[t[i]] = 1 : secondMap[t[i]]+=1;
     }
     
     for (let char in firstMap) {
