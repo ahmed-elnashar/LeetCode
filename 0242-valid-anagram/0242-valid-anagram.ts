9d995ece080982836bb90fe41837fc0af49b550a
Ahmed Tarek El Nashar <a7medelnashar@hotmail.com>
2023-10-07 22:38:36 +0300

    Time: 69 ms (78.43%), Space: 46 MB (48.3%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -0,0 +1,19 @@
+function isAnagram(s: string, t: string): boolean {
+  
+  if (s.length !== t.length) return false;
+  
+  const sMap = {};
+  const tMap = {};
+  
+  for (let i = 0; i < s.length; i++) {
+    sMap[s[i]] = !sMap[s[i]] ? sMap[s[i]] = 1 : sMap[s[i]]+=1;
+    tMap[t[i]] = !tMap[t[i]] ? tMap[t[i]] = 1 : tMap[t[i]]+=1;
+  }
+  
+  for (let char in tMap) {
+    if (tMap[char] !== sMap[char]) return false;
+  }
+  
+  return true;
+  
+};
