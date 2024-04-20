c28bb3dbdab026489601e6dfa88849ee1268987f
Ahmed Tarek El Nashar <a7medelnashar@hotmail.com>
2024-04-20 20:12:40 +0200

    Time: 59 ms (80.48%), Space: 52.6 MB (33.17%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let l = 1;
+    for (let r = 1; r < nums.length; r++) {
+        if (nums[r] !== nums[r-1]) {
+            nums[l] = nums[r]
+            l+=1;
+        }
+    }
+
+    return l;
+    
+};
